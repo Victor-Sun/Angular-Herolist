35f648916c8b8d75ab045127985d95372f9c6d85
Victor Sun <vsun@ostusa.com>
2019-03-25 12:48:28 -0400

    V

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
-
 import { Observable, of } from 'rxjs';
-
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
